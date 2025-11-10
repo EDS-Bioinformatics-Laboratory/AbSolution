8529c02a1c7974fe6f548d49c7a8759865a0a495
RdeBiotec <rodrigo.garcia.valiente@gmail.com>
2025-11-10 01:47:28 +0100

    Version 1.0.0

---
+++ AbSolution/Code/inst/app/www/doc/AbSolution.R
@@ -0,0 +1,15 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval = FALSE
+)
+
+## ----setup_AbS, warning = FALSE, message = FALSE, eval = FALSE, results="hide"----
+#  # Load required package
+#  library(AbSolution)
+#  
+#  # Load Shiny app
+#  AbSolution::run_app()
+#  
+
